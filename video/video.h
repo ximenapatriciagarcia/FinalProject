b1e8f66ccdf11520d0a2719d287b896bda1ad22f
R2D2-Robobot <arturo.cabg07@gmail.com>
2026-06-12 15:18:38 -0600

    Added the rating class + modified video to start working on the movies and series

--- video/video.h
+++ video/video.h
@@ -1,30 +1,28 @@
 #ifndef VIDEO_H
 #define VIDEO_H
-#include "../lenght/Time.h"
+#include "../Time/Time.h"
+#include "../rating/rating.h"
 
 #include <string>
 class Video {
 
     public:
     //-------------GETTERS-------------
-    virtual double getRating()=0;
-    virtual int getID()=0;
-    virtual std::string getName()=0;
-    virtual Time getLength()=0;
+    double GetRating();
+    int GetId();
+    std::string GetName();
+    virtual void ShowLength() = 0;
 
     //------------SETTERS-------------
-    virtual void setRating(double r) = 0;
-    virtual void setID(int id) = 0;
-    virtual void setName(std::string n) = 0;
-    virtual void setLength(Time t) = 0;
+    void SetRating(double r);
+    void SetId(int id);
+    void SetName(std::string n);
+    virtual void SetLength(Time t) = 0;
 
     protected:
-    
-    int ID;
-    Time length;
-    double rating;
+    int id;
+    Rating rate;
     std::string name;
-
     private:
 };
 #endif // VIDEO_H
